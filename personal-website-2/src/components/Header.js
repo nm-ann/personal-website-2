1ed62e651a06e33d56e23a08e2e0d2c588ad4cc9
nm-ann <noam.annenberg@gmail.com>
2018-06-07 22:22:55 -0400

    some UI changes made. More to come.

--- personal-website-2/src/components/Header.js
+++ personal-website-2/src/components/Header.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
+            scrollingLock: false
+        }
+
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll() {
+        if (window.scrollY > 50) {
+            console.log("should lock");
+            this.setState({
+              scrollingLock: true
+            });
+          } else if (window.scrollY < 50) {
+            console.log("not locked" );
+            this.setState({
+              scrollingLock: false
+            });
+          }
+    }
+
     render() {
         return(
             <div className="header">
-                <div className="header-title">
+                <div className="header-title" style={{visibility: this.state.scrollingLock ? "hidden" : "visible"}}>
                     Noam Annenberg
                 </div>
-                <div className="nav-links">
+                <div className="nav-links" ref="navLinks" style={{position: this.state.scrollingLock ? "fixed" : "relative"}}>
                     <a href="#" className="nav-link nav-link-about">About</a>
                     <a href="#" className="nav-link nav-link-projects">Projects</a>
                     <a href="#" className="nav-link nav-link-resume">Resume</a>
